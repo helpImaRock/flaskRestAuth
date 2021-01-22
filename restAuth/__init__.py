4f6c793cb5a9e05a10d0bd6d6647e5d4dad9b9c8
helmpIamRock <50233766+helpImaRock@users.noreply.github.com>
2021-01-22 13:04:56 +0000

    added views and blueprint account operations

--- restAuth/__init__.py
+++ restAuth/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from config import DevelopmentConfig
+from flask_marshmallow import Marshmallow
+from .auth import auth as authbp
 
+ma = Marshmallow()
 
 def create_app(config_file):
 
 	app = Flask(__name__,instance_relative_config=True)
 	app.config.from_object('config.DevelopmentConfig')
+
+	## extensions
+	ma.init_app(app)
+
+	## blueprints
+	app.register_blueprint(authbp,url_prefix='/account')
 
 	return app
---
+++ restAuth/auth/views.py
@@ -0,0 +1,15 @@
+
+
+from . import auth
+
+@auth.route("/login",methods=['POST'])
+def login():
+    pass
+
+@auth.route("/registration",methods=['POST'])
+def register():
+	pass
+
+@auth.route("/<id>",methods=['GET','PUT'])
+def account():
+	pass
